25e7f1f276974ffc44478cd443c91b8c5f916e62
Phillip Boksz <phillip@lunarlogic.io>
2015-02-16 13:44:25 +0100

    Add blank pages for site

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,25 @@
+class HomeController < ApplicationController
+  def information
+
+  end
+
+  def our_story
+
+  end
+
+  def gallery
+
+  end
+
+  def recommendations
+
+  end
+
+  def contact_us
+
+  end
+
+  def rsvp
+
+  end
+end
