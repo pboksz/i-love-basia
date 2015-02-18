1cdb12af716bf11e48f5b21cdd905c1c315c7fe7
Phillip Boksz <phillip@lunarlogic.io>
2015-02-18 16:31:03 +0100

    Add Admin model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218122415) do
+ActiveRecord::Schema.define(version: 20150218143805) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",              limit: 255
+    t.string   "encrypted_password", limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
 
   create_table "guests", force: :cascade do |t|
     t.string   "name",                 limit: 255
