406fa089c1e94851dfb60d00f943f38af91a8d41
Phillip Boksz <phillip@lunarlogic.io>
2015-02-20 13:04:58 +0100

    Set location before each action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
   def after_login_path(locale = nil)
     admin_guests_path
   end
 
   def after_logout_path(locale = nil)
     root_path
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = locale || I18n.default_locale
   end
 end
