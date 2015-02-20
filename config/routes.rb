6668195579b2b91ce3b062c8f02a6281d5f6baf4
Phillip Boksz <phillip@lunarlogic.io>
2015-02-20 12:58:32 +0100

    Add locale to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   admin_auth_routes
 
-  namespace :admin do
-    resources :guests, only: [:index, :new, :create]
+  scope '(:locale)', locale: /(en|pl)/ do
+    namespace :admin do
+      resources :guests, only: [:index, :new, :create]
+    end
+
+    get '/information', to: 'home#information'
+    get '/our_story', to: 'home#our_story'
+    get '/gallery', to: 'home#gallery'
+    get '/recommendations', to: 'home#recommendations'
+    get '/contact_us', to: 'home#contact_us'
+    get '/rsvp', to: 'home#rsvp'
+
+    resource :guests, only: [:show, :create, :update]
+
+    root 'home#information'
   end
-
-  get '/information', to: 'home#information'
-  get '/our_story', to: 'home#our_story'
-  get '/gallery', to: 'home#gallery'
-  get '/recommendations', to: 'home#recommendations'
-  get '/contact_us', to: 'home#contact_us'
-  get '/rsvp', to: 'home#rsvp'
-
-  resource :guests, only: [:show, :create, :update]
-
-  root 'home#information'
 end
