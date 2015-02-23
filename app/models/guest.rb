62491f2640a5243b741d85717718f15d8b4879fb
Phillip Boksz <phillip@lunarlogic.io>
2015-02-23 12:27:58 +0100

    Add styling for groups of guests

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,12 @@
 class Guest < ActiveRecord::Base
   belongs_to :guest_group
   serialize :associated_guest_ids, Array
+
+  def attending?
+    case attending
+      when true; 'Yes'
+      when false; 'No'
+      else; 'No Response'
+    end
+  end
 end
