8d03f806610461ef488037377b68a5eeeea2cd8f
Phillip Boksz <phillip@lunarlogic.io>
2015-02-23 15:47:03 +0100

    Move comments to guest group

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223142229) do
+ActiveRecord::Schema.define(version: 20150223142955) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",              limit: 255
@@ -21,16 +21,16 @@ ActiveRecord::Schema.define(version: 20150223142229) do
   end
 
   create_table "guest_groups", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.text     "comments",   limit: 65535
   end
 
   create_table "guests", force: :cascade do |t|
     t.string   "name",           limit: 255
     t.boolean  "attending",      limit: 1
-    t.text     "comments",       limit: 65535
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.integer  "guest_group_id", limit: 4
   end
 
