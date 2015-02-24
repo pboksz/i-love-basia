ff02c77b5e8742a986d3090a8255a27c54a1fd0f
Phillip Boksz <pboksz@gmail.com>
2015-02-24 14:42:40 +0100

    Add home controller specs

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe HomeController do
+  describe 'GET #information' do
+    before { get :information }
+    it { expect(response).to render_template :information }
+  end
+
+  describe 'GET #our_story' do
+    before { get :our_story }
+    it { expect(response).to render_template :our_story }
+  end
+
+  describe 'GET #gallery' do
+    let(:image_names) { double }
+    let(:image_locator) { double(:image_names => image_names) }
+    before do
+      allow(ImageLocator).to receive(:new).and_return(image_locator)
+      get :gallery
+    end
+
+    it { expect(response).to render_template :gallery, locals: { image_names: image_locator.image_names } }
+  end
+
+  describe 'GET #recommendations' do
+    before { get :recommendations }
+    it { expect(response).to render_template :recommendations }
+  end
+
+  describe 'GET #rsvp' do
+    before { get :rsvp }
+    it { expect(response).to redirect_to guests_path }
+  end
+
+  describe 'GET #contact_us' do
+    before { get :contact_us }
+    it { expect(response).to render_template :contact_us }
+  end
+end
