d62078237714b7333f1cbda4e100043713b58450
Phillip Boksz <phillip@lunarlogic.io>
2015-03-12 16:12:39 +0100

    Add outside link tag specs for map link

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -11,11 +11,24 @@ describe ApplicationHelper do
   end
 
   describe '#outside_link_tag' do
-    subject { helper.outside_link_tag('Text', 'http://link.com') }
+    describe 'without map link' do
+      subject { helper.outside_link_tag('Text', 'http://link.com') }
 
-    it { expect(subject).to include 'href="http://link.com"' }
-    it { expect(subject).to include 'target="_blank"' }
-    it { expect(subject).to include 'Text' }
+      it { expect(subject).to include 'href="http://link.com"' }
+      it { expect(subject).to include 'target="_blank"' }
+      it { expect(subject).to include 'Text' }
+      it { expect(subject).not_to include 'class="map-link"' }
+      it { expect(subject).not_to include 'href="http://map.com"' }
+      it { expect(subject).not_to include 'i class="fa fa-map-marker"' }
+    end
+
+    describe 'with map link' do
+      subject { helper.outside_link_tag('Text', 'http://link.com', map: 'http://map.com') }
+
+      it { expect(subject).to include 'class="map-link"' }
+      it { expect(subject).to include 'href="http://map.com"' }
+      it { expect(subject).to include 'i class="fa fa-map-marker"' }
+    end
   end
 
   describe '#attending_tag' do
@@ -42,7 +55,7 @@ describe ApplicationHelper do
 
     describe 'blank' do
       let(:comments) { '' }
-      it { expect(subject).to include 'title=""' }
+      it { expect(subject).to include '&nbsp;' }
     end
 
     describe 'short comments' do
