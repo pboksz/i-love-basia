3b1e36b2f516447c2f3e939f740417de2ee647d1
Phillip Boksz <phillip@lunarlogic.io>
2015-04-02 15:30:45 +0200

    Add information page features

---
+++ spec/features/information_page_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'Information page' do
+  before { visit information_path }
+
+  it 'has information and countdown' do
+    expect(page).to have_text I18n.t('views.information.header')
+    expect(page).to have_text I18n.t('views.information.countdown.header')
+  end
+end
