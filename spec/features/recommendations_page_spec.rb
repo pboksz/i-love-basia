ca4d9eed3f5f9a2656704a5c30510c4f103bf66a
Phillip Boksz <phillip@lunarlogic.io>
2015-04-07 11:27:17 +0200

    Add recommendations page features

---
+++ spec/features/recommendations_page_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Recommendations page' do
+  before { visit recommendations_path }
+
+  it 'has each section' do
+    expect(page).to have_text I18n.t('views.recommendations.transportation.header')
+    expect(page).to have_text I18n.t('views.recommendations.hotels.header')
+    expect(page).to have_text I18n.t('views.recommendations.restaurants.header')
+    expect(page).to have_text I18n.t('views.recommendations.bars.header')
+    expect(page).to have_text I18n.t('views.recommendations.places.header')
+    expect(page).to have_text I18n.t('views.recommendations.shopping.header')
+    expect(page).to have_text I18n.t('views.recommendations.beauty.header')
+  end
+end
